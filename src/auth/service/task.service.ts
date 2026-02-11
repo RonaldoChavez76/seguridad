de90f61f6abdfaf6bf7528e464d21efa05a3cfde
RonaldoChavez76 <chavezronaldo675@gmail.com>
2026-02-11 17:46:47 -0600

    fix:Estructura funcional del CRUD de tareas y uso de validacines

--- src/auth/service/task.service.ts
+++ src/auth/service/task.service.ts
@@ -1,21 +1,42 @@
 import { Injectable } from "@nestjs/common";
+import { CreateTaskDto } from "../dto/create-task.dto";
 
 @Injectable()
 export class TaskService {
-  public getTasks(): string[] {
-    return ['Task 1', 'Task 2', 'Task 3'];
+
+  private tasks: any[] = [];
+
+  public getTasks(): any[] {
+    return this.tasks;
   }
 
-  public getTaskById(id: number): string {
-    return 'Se obtiene la tarea ${id}';
-  }
-  public insertTask(task: any): any {
+  public getTaskById(id: number): any {
+    var task = this.tasks.filter((data) => data.id === id);
     return task;
   }
+
+  public insertTask(task: CreateTaskDto): any {
+    var id = this.tasks.length + 1;
+    var position =this.tasks.push({ id, ...task });
+    return this.tasks[position - 1];
+  }
+
   public updateTask(id: number, task: any): any {
-    return task;
+    const taskUpdated = this.tasks.find((data) => {
+      if (data.id === id) {
+          if(task.name) data.name = task.name;
+          if(task.description) data.description = task.description; 
+          if(task.priority != null) data.priority = task.priority;
+
+          return data;
+      }
+    });
+    return taskUpdated;
   }
+
   public deleteTask(id: number): any {
-    return 'Eliminando la tarea ' + id;
+    const array = this.tasks.filter((data) => data.id !== id);
+    this.tasks = array;
+    return `Task deleted`;
   }
 }
